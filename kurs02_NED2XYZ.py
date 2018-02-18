26ea1ef45f2aa2dc061ca933f031914226706f54
boelek <boelek@users.noreply.github.com>
2018-02-18 16:47:13 +0100

    second session python script
    Open a text file, read each line, split line by delimiter and convert. Store result in a new list.

---
+++ kurs02_NED2XYZ.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 18 06:40:34 2018
+
+@author: boele
+"""
+
+# training 02, reading text files and converting from NED to XYZ
+
+# open xyz file
+f = open('test.xyz', 'r')
+data = f.read()
+xyz = data.split('\n')
+
+# print first 5 rows
+print(xyz[0:5])
+
+#  create empty ned list
+ned = []
+
+# for each row convert xyz to ned and append to ned list
+for row in xyz:
+    values = row.split(' ')
+    ned.append([values[1], values[0], '-'+values[2]])
+
+#print first 5 ned rows
+print(ned[0:5])
+
+
+# print depth at row 15
+print(ned[16][2])
